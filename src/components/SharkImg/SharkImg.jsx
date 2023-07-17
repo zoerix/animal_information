01c05b0e2ec3cc0ce38ccdf8033433a887dcf322
zoerix <zoe.rix.z@gmail.com>
2023-07-17 22:36:33 +0200

    now shark img?!

--- src/components/SharkImg/SharkImg.jsx
+++ src/components/SharkImg/SharkImg.jsx
@@ -1,8 +1,8 @@
 const SharkImg = () => {
-    const sharkImgUrl = 'src/Img/hai-3347787_1280.jpg'
+    const sharkImgUrl = 'https://images.unsplash.com/photo-1583265226154-43beba9afab4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTZ8fHNoYXJrfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
     return (
         <div className="ImgContainer">
-            <img src= {sharkImgUrl} alt="PlaceholderShark" />
+            <img src={sharkImgUrl} alt="PlaceholderShark" />
         </div>
     )
 }
